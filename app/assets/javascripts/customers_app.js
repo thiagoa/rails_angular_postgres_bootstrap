1d0e04358fc329f7c73e5eed406494b7c2910bb5
Thiago A. Silva <thiagoaraujos@gmail.com>
2016-04-24 15:43:58 -0300

    Add pagination enable / disable feature to book's code
    Also refactor code.

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,39 +1,98 @@
 var app = angular.module('customers', []);
 
 var CustomerSearchController = function($scope, $http) {
-  var page = 0;
+  var page = 0, perPage = 10;
 
-  $scope.customers = [];
+  function initializeScope() {
+    $scope.search = searchCustomers;
+    $scope.previousPage = previousPage;
+    $scope.nextPage = nextPage;
+    $scope.searchHasResults = searchHasResults;
 
-  $scope.search = function(searchTerm) {
-    var params;
+    hidePagination();
+  }
+
+  function hidePagination() {
+    $scope.previousPageDisabled = true;
+    $scope.nextPageDisabled = true;
+  }
+
+  function initialize() {
+    initializeScope();
+    noCustomers();
+  }
+
+  function noCustomers() {
+    $scope.customers = [];
+  }
+
+  function updateCustomers(data) {
+    $scope.previousPageDisabled = isBeginningOfPagination();
+    $scope.nextPageDisabled = isEndOfPagination(data.count);
+
+    $scope.customers = data.records;
+  }
+
+  function customersFetchError(data, status) {
+    alert('There was a problem: ' + status);
+  }
+
+  function isBeginningOfPagination() {
+    return (page === 0);
+  }
+
+  function isEndOfPagination(recordCount) {
+    return perPage + (perPage * page) >= recordCount;
+  }
+
+  function fetchCustomers(params) {
+    $http.get('/customers.json', { params: params })
+      .success(updateCustomers)
+      .error(customersFetchError);
+  }
+
+  function searchCustomers(searchTerm, localPage) {
+    if (localPage === undefined) {
+      page = localPage = 0;
+    }
 
     if (searchTerm.length < 3) {
+      hidePagination();
+      return noCustomers();
+    }
+
+    fetchCustomers({
+      keywords: searchTerm,
+      page: localPage,
+      per_page: perPage
+    });
+  }
+
+  function previousPage() {
+    if ($scope.previousPageDisabled) {
       return;
     }
 
-    params = { keywords: searchTerm, page: page }
-
-    $http.get('/customers.json', { params: params })
-      .success(function(data, status, headers, config) {
-        $scope.customers = data;
-      })
-      .error(function(data, status, headers, config) {
-        alert('There was a problem: ' + status);
-      });
-  };
-
-  $scope.previousPage = function() {
     if (page > 0) {
       page = page - 1;
-      $scope.search($scope.keywords);
+      $scope.search($scope.keywords, page);
     }
-  };
-
-  $scope.nextPage = function() {
-    page = page + 1;
-    $scope.search($scope.keywords);
   }
+
+  function nextPage() {
+    if ($scope.nextPageDisabled) {
+      return;
+    }
+
+    page = page + 1;
+    $scope.search($scope.keywords, page);
+  }
+
+  function searchHasResults() {
+    return !$scope.previousPageDisabled || !$scope.nextPageDisabled;
+  }
+
+  initialize();
 };
 
 app.controller(
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,29 +1,33 @@
 class CustomersController < ApplicationController
-  PAGE_SIZE = 10
-
   def index
-    @page = (params[:page] || 0).to_i
+    page = (params[:page] || 0).to_i
+    per_page = (params[:per_page] || 10).to_i
 
     if params[:keywords].present?
-      @keywords = params[:keywords]
+      keywords = params[:keywords]
 
-      customer_search_term = CustomerSearchTerm.new(@keywords)
+      customer_search_term = CustomerSearchTerm.new(keywords)
 
-      @customers  = Customer.where(
+      customer_scope = Customer.where(
         customer_search_term.where_clause,
         customer_search_term.where_args
       )
+
+      count = customer_scope.count
+      customers = customer_scope
         .order(customer_search_term.order)
-        .offset(PAGE_SIZE * @page)
-        .limit(PAGE_SIZE)
+        .offset(per_page * page)
+        .limit(per_page)
 
     else
-      @customers = []
+      customers = []
     end
 
     respond_to do |format|
       format.html {}
-      format.json { render json: @customers }
+      format.json do
+        render json: { count: count, records: customers }
+      end
     end
   end
 end
