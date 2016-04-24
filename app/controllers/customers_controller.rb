516608f090553546a1e3ffc61e022b154026992d
Thiago A. Silva <thiagoaraujos@gmail.com>
2016-04-24 12:37:55 -0300

    Finish chapter 4

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,9 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
+
   def index
+    @page = (params[:page] || 0).to_i
+
     if params[:keywords].present?
       @keywords = params[:keywords]
 
@@ -8,7 +12,11 @@ class CustomersController < ApplicationController
       @customers  = Customer.where(
         customer_search_term.where_clause,
         customer_search_term.where_args
-      ).order(customer_search_term.order)
+      )
+        .order(customer_search_term.order)
+        .offset(PAGE_SIZE * @page)
+        .limit(PAGE_SIZE)
+
     else
       @customers = []
     end
